34afc6d689df18aaaa9bb0e710d8546d6de57862
Os-carsun <good0991@gmail.com>
2016-11-15 09:48:42 -0800

    update makefile

--- comp.sh
+++ comp.sh
@@ -2,8 +2,12 @@
 
 prefix="commands/"
 postfix=".cpp"
+mkdir -p runtime
+mkdir -p runtime/bin
 for f in commands/*.cpp; do
     name=${f#$prefix}
     name=${name%$postfix}
     g++ $f -o  runtime/bin/$name
 done
+cp /bin/ls runtime/bin/ls
+cp /bin/cat runtime/bin/cat
